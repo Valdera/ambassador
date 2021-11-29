84a09ce4a134d58cf9e14a0d48ba708a9453c6df
Valdera <f.valdera@yahoo.co.id>
2021-11-29 16:11:58 +0700

    Go Admin

---
+++ src/database/db.go
@@ -0,0 +1,27 @@
+package database
+
+import (
+	"ambassador/src/models"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	var err error
+	DB, err = gorm.Open(mysql.Open("root:root@tcp(db:3306)/ambassador"), &gorm.Config{})
+
+	if err != nil {
+		panic("Could not connect with the database!")
+	}
+}
+
+func AutoMigrate() {
+	err := DB.AutoMigrate(models.User{}, models.Product{}, models.Link{}, models.Order{}, models.OrderItem{})
+
+	if err != nil {
+		panic("Could not auto migrate models with the database!")
+
+	}
+}
